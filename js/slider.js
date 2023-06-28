318c150e63856fc54f453b7590dc7e86aff496f9
Sonu <sonu0696.be20@chitkara.edu.in>
2023-06-28 12:00:37 +0530

    NGO_WEBSITE

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+
+		
+// scroll top
+    $(document).ready(function(){
+		$("#scrolltop").hide();
+		$(window).scroll(function(){
+			var x=$(document).scrollTop();
+			if(x>200){
+				$("#scrolltop").show("slow");
+			}
+			else{
+				$("#scrolltop").hide("slow");
+			}
+		});
+        $("#scrolltop").click(function(){
+			$(window).scrollTop(0);
+		});
+	});
